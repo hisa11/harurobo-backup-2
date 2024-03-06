a43c442a0e562721545dc82dc4014af1abdd2236
hisa11 <132685722+hisa11@users.noreply.github.com>
2024-03-06 23:58:45 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ int main()
     SolenoidController solenoidController(can, pc);
     ServoController servoController(can, pc);
 
-    char command[7];                // Command and number combination array
+    char command[1];                // Command and number combination array
     int16_t pwm0[4] = {0, 0, 0, 0}; // Motor control array
 
     auto pre = timer.elapsed_time(); // Previous time
@@ -50,158 +50,154 @@ int main()
             {
                 idx += n;
             }
+            // break;
         }
         command[idx] = '\0';   // Add string terminator
         printf("%s", command); // Print received command
 
         // Process motor control commands
         char buf;
-        pc.read(&buf, sizeof(buf));
-        pc.write(&buf, sizeof(buf));
-        switch (buf)
-        {
-         case 'D':
-            penguin.pwm[0] = 10000;
-            penguin.pwm[1] = 0;
-            penguin.pwm[2] = 0;
-            break;
-        case 'd':
-            penguin.pwm[0] = -10000;
-            penguin.pwm[1] = 0;
-            penguin.pwm[2] = 0;
-            break;
-        case 'f':
-            penguin.pwm[0] = 0;
-            penguin.pwm[1] = 0;
-            penguin.pwm[2] = 5000;
-            break;
-        case 'g':
-            penguin.pwm[0] = 0;
-            penguin.pwm[1] = 0;
-            penguin.pwm[2] = -50000;
-            break;
-        case 'i':
-        if(button1 == 1){
-            penguin.pwm[1] = 3000;
-            penguin.pwm[0] = 0;
-            penguin.pwm[2] = 0;
-        }
-        else{
-            penguin.pwm[1] = 0;
-            penguin.pwm[0] = 0;
-            penguin.pwm[2] = 0;
-        }
-            break;
-        case 'y':
-        if (button2 == 1)
-        {
-            penguin.pwm[1] = -3000;
-            penguin.pwm[0] = 0;
-            penguin.pwm[2] = 0;
-        }
-        else{
-            penguin.pwm[0] = 0;
-            penguin.pwm[1] = 0;
-            penguin.pwm[2] = 0;
-        }
-            break;
-        
-        default:
-            penguin.pwm[0] = 0;
-            penguin.pwm[1] = 0;
-            penguin.pwm[2] = 0;
-            break;
-        }
-        switch (buf)
-        {
-        case 'Z':
-            number_r = 120;
-            break;
-        case 'X':
-            number_r = 90;
-            break;
-        case 'C':
-            number_r = 30;
-            break;
-        case 'V':
-            number_r = -30;
-            break;
-        case 'B':
-            number_r = -90;
-            break;
-        case 'N':
-            number_r = -120;
-            break;
-        default:
-            number_r = 0;
-            break;
-        }
-        switch (buf)
-        {
-        case 'z':
-            number_R = -120;
-            break;
-        case 'x':
-            number_R = -60;
-            break;
-        case 'c':
-            number_R = -30;
-            break;
-        case 'v':
-            number_R = 30;
-            break;
-        case 'b':
-            number_R = 60;
-            break;
-        case 'n':
-            number_R = 120;
-            break;
-        default:
-            number_R = 0;
-            break;
-        }
-        switch (buf)
-        {
-        case '1':
-            number_L = 90;
-            break;
-        case '2':
-            number_L = 60;
-            break;
-        case '3':
-            number_L = 30;
-            break;
-        case '4':
-            number_L = -30;
-            break;
-        case '5':
-            number_L = -60;
-            break;
-        case '6':
-            number_L = -90;
-            break;
-        default:
-            number_L = 0;
-            break;
+        int read_result = pc.read(&buf, sizeof(buf));
+        if (read_result > 0) {
+            pc.write(&buf, sizeof(buf));
+            switch (buf) {
+                case 'D':
+                    penguin.pwm[0] = 17000;
+                    penguin.pwm[1] = 0;
+                    penguin.pwm[2] = 0;
+                    penguin.send();
+                    break;
+                case 'd':
+                    penguin.pwm[0] = -17000;
+                    penguin.pwm[1] = 0;
+                    penguin.pwm[2] = 0;
+                    penguin.send();
+                    break;
+                case 'f':
+                    penguin.pwm[0] = 0;
+                    penguin.pwm[1] = 0;
+                    penguin.pwm[2] = 5000;
+                    penguin.send();
+                    break;
+                case 'g':
+                    penguin.pwm[0] = 0;
+                    penguin.pwm[1] = 0;
+                    penguin.pwm[2] = -5000;
+                    penguin.send();
+                    break;
+                case 'i':
+                    if (button2 == 1)
+                    {
+                        penguin.pwm[0] = 3000;
+                        penguin.pwm[3] = 0;
+                        penguin.pwm[2] = 0;
+                    }
+                    else
+                    {
+                        penguin.pwm[1] = 0;
+                        penguin.pwm[0] = 0;
+                        penguin.pwm[2] = 0;
+                    }
+                    penguin.send();
+                    break;
+                case 'y':
+                    if (button1 == 1)
+                    {
+                        penguin.pwm[1] = -3000;
+                        penguin.pwm[0] = 0;
+                        penguin.pwm[2] = 0;
+                    }
+                    else
+                    {
+                        penguin.pwm[0] = 0;
+                        penguin.pwm[1] = 0;
+                        penguin.pwm[2] = 0;
+                    }
+                    penguin.send();
+                    break;
+
+                default:
+                    penguin.pwm[0] = 0;
+                    penguin.pwm[1] = 0;
+                    penguin.pwm[2] = 0;
+                    penguin.send();
+                    break;
+            }
+            switch (buf)
+            {
+                case 'Z':
+                    number_r = 120;
+                    break;
+                case 'X':
+                    number_r = 90;
+                    break;
+                case 'C':
+                    number_r = 30;
+                    break;
+                case 'V':
+                    number_r = -30;
+                    break;
+                case 'B':
+                    number_r = -90;
+                    break;
+                case 'N':
+                    number_r = -120;
+                    break;
+                case 'M':
+                    number_r = 0;
+                    break;
+            }
+            switch (buf)
+            {
+                case 'z':
+                    number_R = -120;
+                    break;
+                case 'x':
+                    number_R = -60;
+                    break;
+                case 'c':
+                    number_R = -30;
+                    break;
+                case 'v':
+                    number_R = 30;
+                    break;
+                case 'b':
+                    number_R = 60;
+                    break;
+                case 'n':
+                    number_R = 120;
+                    break;
+                case 'm':
+                    number_R = 0;
+                    break;
+            }
+            switch (buf)
+            {
+                case '1':
+                    number_L = 90;
+                    break;
+                case '2':
+                    number_L = 60;
+                    break;
+                case '3':
+                    number_L = 30;
+                    break;
+                case '4':
+                    number_L = -30;
+                    break;
+                case '5':
+                    number_L = -60;
+                    break;
+                case '6':
+                    number_L = -90;
+                    break;
+                case '0':
+                    number_L = 0;
+                    break;
+            }
         }
 
         // Calculate motor PWM values
-            penguin.send();
-            penguin.pwm[0] = 0;
-            penguin.pwm[1] = 0;
-            penguin.pwm[2] = 0;
-
-        
-        pwm0[0] = static_cast<int16_t>(((number_r - number_R) * 90 - number_L * 30));
-        pwm0[1] = static_cast<int16_t>(((number_r + number_R) * 90 - number_L * 30));
-        pwm0[2] = static_cast<int16_t>(-((number_r + number_R) * 90 + number_L * 30));
-        pwm0[3] = static_cast<int16_t>(-((number_r - number_R) * 90 + number_L * 30));
-
-        // Process motor speed control
-        // Send motor PWM values via CAN
-        CANMessage msg0(1, (const uint8_t *)pwm0, 8);
-        can.write(msg0);
-        printf("pwm: %d\n", pwm0);
 
         // Read feedback from CAN
         if (can.read(msg))
@@ -245,5 +241,15 @@ int main()
                 }
             }
         }
+        pwm0[0] = static_cast<int16_t>(((number_r - number_R) * 90 - number_L * 30));
+        pwm0[1] = static_cast<int16_t>(((number_r + number_R) * 90 - number_L * 30));
+        pwm0[2] = static_cast<int16_t>(-((number_r + number_R) * 90 + number_L * 30));
+        pwm0[3] = static_cast<int16_t>(-((number_r - number_R) * 90 + number_L * 30));
+
+        // Process motor speed control
+        // Send motor PWM values via CAN
+        CANMessage msg0(1, (const uint8_t *)pwm0, 8);
+        can.write(msg0);
+        printf("pwm: %d\n", pwm0);
     }
 }
